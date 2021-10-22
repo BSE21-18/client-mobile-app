fba89eab6bb4b64897a26a6babb91e41f97eae16
Amoko Ivan <amokoivan@gmail.com>
2021-10-22 12:05:38 +0300

    Android app design

---
+++ app/src/main/java/com/amotech/datavoc/Activate.kt
@@ -0,0 +1,12 @@
+package com.amotech.datavoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activate)
+
+    }
+}
